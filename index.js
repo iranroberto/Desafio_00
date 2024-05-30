d2e6a5d96b95a543cd2ccce33d5222ca9f35b3e2
iranroberto <irandev.oficial@gmail.com>
2024-05-30 09:53:29 -0300

    desfy

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-Dev
+let nomeHeroi = "Super Alegria"
+
+
+let forcaXp = 103384
+
+let nivel
+
+if (forcaXp < 1000 ) {
+    nivel = "Ferro"
+}
+else if (forcaXp >1001 && forcaXp < 2000) {
+    nivel = "Bronze"
+}
+else if (forcaXp >2001 && forcaXp < 5000) {
+    nivel = "Prata"
+}
+else if (forcaXp >5001 && forcaXp < 7000) {
+    nivel = "Ouro"
+}
+else if (forcaXp >7001 && forcaXp <8000) {
+    nivel = "Platina"
+}
+else if (forcaXp >8001 && forcaXp <9000) {
+    nivel = "Ascendente"
+}
+else if (forcaXp >9001 && forcaXp <10000) {
+    nivel = "Imortal"
+}
+else { (forcaXp >= 10001)
+    nivel = "Radiante"
+}
+console.log("")
+
+console.log (" O heroi " +nomeHeroi + " está no nivel " +nivel+ " .")
+console.log("")
+
+
+
+
